e29e12d39d43178e5193fc42f14e48288f02e06a
LTPhat <102889533+LTPhat@users.noreply.github.com>
2022-06-30 14:31:54 +0700

    master

---
+++ First_Unique_Character.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def firstUniqChar(self, s):
+        mydict = dict()
+        for i in s:
+            if i in mydict:
+                mydict[i] += 1
+            else:
+                mydict[i] = 1
+        for i in range (len(s)):
+            if mydict[s[i]] == 1:
+                return i
+        return -1
+def main():
+    sol = Solution()
+    print(sol.firstUniqChar('leetcode'))
+main()
